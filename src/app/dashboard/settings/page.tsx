fff76bc34a6a9af70ce106bf41ffa7131cc2270f
Favour <gismozfexie@yahoo.com>
2025-04-27 19:33:56 +0100

    Feat/add user wallet and settings page (#27)
    * feat: add wallet page title and summary card
    
    * chore: add shadcn table component
    
    * feat: add transaction history table
    
    add transaction table with search implementation
    
    * chore: add table empty state + make table headers more dynamic
    
    * chore: minor responsivity mods
    
    make seach component full width on mobile
    
    * feat: add settings page
    
    * chore: move components to seperate file

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,3 +1,143 @@
+'use client';
+import CopyToClipboard from "@/components/dashboard/copy-to-clipboard";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import ThemeSelect from "@/components/dashboard/settings/theme-select";
+import TokenPreference from "@/components/dashboard/settings/token-preference";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Switch } from "@/components/ui/switch"
+import { useState } from "react";
+
 export default function Settings() {
-  return <h2>settings</h2>;
+  const [form, setForm] = useState({
+    theme: "dark",
+    name: "",
+    email: "",
+    language: "en",
+    bio: "",
+    tokenPreference: "USDC",
+    emailAlerts: true,
+    inAppNotifications: false,
+  })
+
+  return (
+    <div className="text-white space-y-12">
+      <h1 className="text-2xl md:text-4xl font-semibold text-white">Settings</h1>
+
+      <section className="space-y-3">
+        <div className="space-y-2.5">
+          <span className="block text-xs">Public profile link</span>
+          <CopyToClipboard
+            text="www.coinmarletcap.com"
+            onCopied={() => alert("Copied to clipboard!")}
+            className="md:max-w-sm"
+          />
+        </div>
+
+        <div className="space-y-2.5">
+          <span className="block text-xs">Theme</span>
+          <ThemeSelect
+            value={form.theme}
+            onChange={(value => setForm({ ...form, theme: value }))}
+          />
+        </div>
+
+        <div className="grid md:grid-cols-2 gap-2.5 md:gap-4 md:max-w-2/3">
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="name" className="text-xs">Display Name</Label>
+            <Input
+              type="text"
+              id="name"
+              placeholder="Name"
+              className="border-[#515461]"
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+              value={form.name}
+            />
+          </div>
+
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="email" className="text-xs">Email address (for Notification)</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="border-[#515461]"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              value={form.email}
+            />
+          </div>
+
+          <Label className="flex flex-col items-start gap-2">
+            <span className="text-xs">Language</span>
+            <Select
+              value={form.language}
+              onValueChange={(value) => setForm({ ...form, language: value })}>
+              <SelectTrigger className="w-full border-[#515461]">
+                <SelectValue placeholder="Language" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Language</SelectLabel>
+                  <SelectItem value="en">English</SelectItem>
+                  <SelectItem value="fr">French</SelectItem>
+                  <SelectItem value="es">Spanish</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </Label>
+
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="bio">Description / Bio</Label>
+            <Input
+              type="text"
+              id="bio"
+              placeholder="Description / Bio"
+              className="border-[#515461]"
+              onChange={(e) => setForm({ ...form, bio: e.target.value })}
+              value={form.bio}
+            />
+          </div>
+
+          <div className="md:col-span-2 grid w-full items-center gap-2">
+            <Label htmlFor="bio">Default Token Preference</Label>
+            <TokenPreference
+              value={form.tokenPreference}
+              options={["USDC", "STRK"]}
+              onChange={(value) => setForm({ ...form, tokenPreference: value })}
+            />
+          </div>
+
+          <div className="col-start-1 flex items-center justify-between gap-2"> 
+            <Label htmlFor="email-alerts">Toggle Email Alerts</Label>
+            <Switch
+              id="email-alerts"
+              className="border-2 border-white data-[state=checked]:border-[#259BA5]
+              !bg-transparent data-[state=checked]:!bg-[#259BA5]"
+              checked={form.emailAlerts}
+              onCheckedChange={(checked) => setForm({ ...form, emailAlerts: checked })}
+            />
+          </div>
+
+          <div className="col-start-1 flex items-center justify-between gap-2"> 
+            <Label htmlFor="in-app-notif">Toggle In-App Notifications</Label>
+            <Switch
+              id="in-app-notif"
+              className="border-2 border-white data-[state=checked]:border-[#259BA5]
+              !bg-transparent data-[state=checked]:!bg-[#259BA5]"
+              checked={form.inAppNotifications}
+              onCheckedChange={(checked) => setForm({ ...form, inAppNotifications: checked })}
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
--- src/app/dashboard/wallet/page.tsx
+++ src/app/dashboard/wallet/page.tsx
@@ -1,3 +1,87 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon, ArrowUpRight } from "lucide-react";
+import SummaryCard from "@/components/dashboard/wallet/summary-card";
+import TransactionSummary from "@/components/dashboard/wallet/transaction-summary";
+
+// TODO: remove sample data when API is ready
+const summaryCardData = [
+  {
+    title: "Current Balance",
+    amount: "$1,205",
+  },
+  {
+    title: "Locked Token",
+    amount: "$1,205",
+  },
+  {
+    title: "Claimable Token",
+    amount: "$1,205",
+  }
+]
+
+const transactionHistory = [
+  {
+    date: "2h 38secs",
+    action: "Yes",
+    poolName: "Will BTC reach $50K by June?",
+    amountStaked: "$4.50",
+    status: "active",
+  },
+  {
+    date: "3h 12mins",
+    action: "No",
+    poolName: "Will DOGE reach $1 by March?",
+    amountStaked: "$10.50",
+    status: "active",
+  },
+  {
+    date: "6h 12mins",
+    action: "Yes",
+    poolName: "How many ETH will be staked by 2024?",
+    amountStaked: "$2.50",
+    status: "active",
+  },
+]
+
 export default function Wallet() {
-  return <h2>hello</h2>;
+  return (
+    <div className="text-white space-y-12">
+      <section className="space-y-5">
+        <header className="flex gap-4 max-md:flex-col justify-between md:items-center">
+          <h1 className="text-2xl md:text-4xl font-semibold text-white">Wallet Analysis</h1>
+          <div className="flex gap-x-2">
+            <Button
+              className="bg-[#259BA5] text-black font-medium hover:bg-[#259BA5]/80
+              transition-colors disabled:opacity-50 space-x-2.5">
+              Deposit <PlusIcon className="w-4" />
+            </Button>
+
+            <Button
+              variant="ghost"
+              className="hover:bg-transparent hover:text-white"
+            >
+              Withdrawal <ArrowUpRight className="w-4" />
+            </Button>
+
+            <Button
+              variant="ghost"
+              className="flex items-center gap-2.5 hover:bg-transparent hover:text-white"
+            >
+              Rewards <span className="bg-[#259BA5] text-black px-1 rounded-full py-px">3</span>
+            </Button>
+          </div>
+        </header>
+
+        <div className="grid md:grid-cols-3 gap-2.5">
+          {summaryCardData.map((card, index) => (
+            <SummaryCard key={index} title={card.title} amount={card.amount} />
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <TransactionSummary data={transactionHistory} />
+      </section>
+    </div>
+  )
 }
